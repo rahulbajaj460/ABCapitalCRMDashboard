038a85c60b4e5531d4936ededf7923406794b7bc
Bajaj <rahulbajajmps@gmail.com>
2026-05-31 16:26:32 +0530

    fix tiptap table imports

--- src/components/Wiki.jsx
+++ src/components/Wiki.jsx
@@ -6,10 +6,10 @@ import Underline from "@tiptap/extension-underline";
 import TextAlign from "@tiptap/extension-text-align";
 import Link from "@tiptap/extension-link";
 import Placeholder from "@tiptap/extension-placeholder";
-import Table from "@tiptap/extension-table";
-import TableRow from "@tiptap/extension-table-row";
-import TableCell from "@tiptap/extension-table-cell";
-import TableHeader from "@tiptap/extension-table-header";
+import { Table } from "@tiptap/extension-table";
+import { TableRow } from "@tiptap/extension-table-row";
+import { TableCell } from "@tiptap/extension-table-cell";
+import { TableHeader } from "@tiptap/extension-table-header";
 import { supabase } from "../supabase";
 
 // Toolbar button component
